94a02d94638c3fae8fa434a1a557dc09f68c1bff
Prince <princekumar.priku@gmail.com>
2024-01-19 11:14:03 +0530

    migrating from h2 database to mysql database

--- src/main/java/com/learn/repository/HolidayRepository.java
+++ src/main/java/com/learn/repository/HolidayRepository.java
@@ -15,7 +15,7 @@ public class HolidayRepository {
     private JdbcTemplate jdbcTemplate;
 
     public List<Holiday> findAllHolidays() {
-        String sql = "SELECT * FROM HOLIDAYS";
+        String sql = "select * from holidays";
         var rowMapper = BeanPropertyRowMapper.newInstance(Holiday.class);
         return jdbcTemplate.query(sql, rowMapper);
     }
